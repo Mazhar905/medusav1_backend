c4a19c1f17dfd4a29770ddf232b132772d9ae3fb
mazhar <--get>
2024-08-06 15:53:08 +0500

    misc changes

--- medusa-config.js
+++ medusa-config.js
@@ -91,7 +91,6 @@ const plugins = [
 		options: {
 			serve: true,
 			autoRebuild: true,
-			backend: "https://admin.egalaspot.com",
 			path: "/app",
 			outDir: "build",
 			develop: {
@@ -108,18 +107,18 @@ const plugins = [
 ];
 
 const modules = {
-	// eventBus: {
-	// 	resolve: "@medusajs/event-bus-redis",
-	// 	options: {
-	// 		redisUrl: REDIS_URL,
-	// 	},
-	// },
-	// cacheService: {
-	// 	resolve: "@medusajs/cache-redis",
-	// 	options: {
-	// 		redisUrl: REDIS_URL,
-	// 	},
-	// },
+	eventBus: {
+		resolve: "@medusajs/event-bus-redis",
+		options: {
+			redisUrl: REDIS_URL,
+		},
+	},
+	cacheService: {
+		resolve: "@medusajs/cache-redis",
+		options: {
+			redisUrl: REDIS_URL,
+		},
+	},
 };
 
 /** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
@@ -130,7 +129,15 @@ const projectConfig = {
 	database_url: DATABASE_URL,
 	admin_cors: ADMIN_CORS,
 	// Uncomment the following lines to enable REDIS
-	// redis_url: REDIS_URL,
+	redis_url: REDIS_URL,
+	database_extra:
+		process.env.NODE_ENV !== "development"
+			? {
+					ssl: {
+						rejectUnauthorized: false,
+					},
+			  }
+			: {},
 };
 
 /** @type {import('@medusajs/medusa').ConfigModule} */
